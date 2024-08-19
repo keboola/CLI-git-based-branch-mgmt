328ebd1a298fda333dd0fb3d023355658f3a45da
David Esner <david@keboola.com>
2024-08-19 10:40:46 +0200

    gh tools

--- .github/actions/kbc_set_branch_id/src/set_branch.py
+++ .github/actions/kbc_set_branch_id/src/set_branch.py
@@ -1,4 +1,17 @@
+import json
 import os
+
+import github_action_utils as gh_utils
+
+MANIFEST_PATH = '.keboola/manifest.json'
+BRANCH_MAPPING_PATH = '.keboola/branch-mapping.json'
+
+
+def get_api_host() -> str:
+    with open(MANIFEST_PATH, 'r') as f:
+        manifest = json.load(f)
+        return manifest['project']['apiHost']
+
 
 def list_files(startpath):
     for root, dirs, files in os.walk(startpath):
@@ -9,5 +22,11 @@ def list_files(startpath):
         for f in files:
             print('{}{}'.format(subindent, f))
 
+
+def get_current_git_branch_name() -> str:
+    return gh_utils.get_env('GITHUB_REF_NAME')
+
+
 list_files('.')
 print(os.environ)
+print(gh_utils.get_workflow_environment_variables())
